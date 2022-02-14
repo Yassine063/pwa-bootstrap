b137f07b1e1080f38ee98d73620ef3f1cec99aa5
Yassine063 <yas-elkhaddioui@hotmail.fr>
2022-02-13 23:39:23 -0500

    test3

--- sw.js
+++ sw.js
@@ -2,10 +2,10 @@ this.addEventListener('install', function(event) {
     event.waitUntil(
       caches.open('v1').then(function(cache) {
         return cache.addAll([
-
           '/pwa-bootstrap/',
-          '/pwa-bootstrap/index.html',
           '/pwa-bootstrap/index.js',
+          '/pwa-bootstrap/index.html',
+          '/pwa-bootstrap/page hors connexion.html',
           '/pwa-bootstrap/mobile2.jpg',
           '/pwa-bootstrap/ordinateur.png',
           '/pwa-bootstrap/tablettes.jpg',
