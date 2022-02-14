dffde0bc20eadcb6a1303a72fa76c7a013c92644
Yassine063 <yas-elkhaddioui@hotmail.fr>
2022-02-14 00:30:10 -0500

    test6

--- sw.js
+++ sw.js
@@ -2,21 +2,21 @@ this.addEventListener('install', function(event) {
     event.waitUntil(
       caches.open('v1').then(function(cache) {
         return cache.addAll([
-          '/pwa-bootstrap/',
-          '/pwa-bootstrap/index.js',
-          '/pwa-bootstrap/index.html',
-          '/pwa-bootstrap/page hors connexion.html',
-          '/pwa-bootstrap/mobile2.jpg',
-          '/pwa-bootstrap/ordinateur.png',
-          '/pwa-bootstrap/tablettes.jpg',
-          '/pwa-bootstrap/icon1.png',
-          '/pwa-bootstrap/icon-192x192.png',
-          '/pwa-bootstrap/icon-256x256.png',
-          '/pwa-bootstrap/icon-384x384.png',
-          '/pwa-bootstrap/icon-512x512.png',
-          '/pwa-bootstrap/bootstrap-5.1.3-dist/js/bootstrap.bundle.min.js',
-          '/pwa-bootstrap/bootstrap-5.1.3-dist/css/bootstrap.min.css',
-          '/pwa-bootstrap/icons-1.7.2/font/bootstrap-icons.css'
+          '/',
+          '/index.js',
+          '/index.html',
+          '/page hors connexion.html',
+          '/mobile2.jpg',
+          '/ordinateur.png',
+          '/tablettes.jpg',
+          '/icon1.png',
+          '/icon-192x192.png',
+          '/icon-256x256.png',
+          '/icon-384x384.png',
+          '/icon-512x512.png',
+          '/bootstrap-5.1.3-dist/js/bootstrap.bundle.min.js',
+          '/bootstrap-5.1.3-dist/css/bootstrap.min.css',
+          '/icons-1.7.2/font/bootstrap-icons.css'
           
             
         ]);
